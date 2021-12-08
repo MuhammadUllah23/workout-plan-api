96f32b626629632f86602e0b1f7687cd15f19b69
Muhammad <muhammadhullah23@gmail.com>
2021-12-07 22:11:58 -0500

    create table with workout, exercise and session

---
+++ db/migrate/20211208025519_create_exercises.rb
@@ -0,0 +1,11 @@
+class CreateExercises < ActiveRecord::Migration[6.1]
+  def change
+    create_table :exercises do |t|
+      t.string :name
+      t.string :target
+      t.belongs_to :workout, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

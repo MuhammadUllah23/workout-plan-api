fa7a1df5e6dfde80f2f1d8bd6cdfdaff9213c76e
Muhammad <muhammadhullah23@gmail.com>
2021-12-12 04:36:58 -0500

    exercise create action

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -4,4 +4,14 @@ class ExercisesController < ApplicationController
         render json: exercises
     end
 
+    def create
+        @exercise = Exercise.new(exercise_params)
+        # byebug
+        if @exercise.save
+            render json: @exercise, status: :created, location: @exercise
+        else
+            render json: {message: "Make sure to fill all inputs"}
+        end
+    end
+
 end

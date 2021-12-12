70ce8fef2dce7f7e9c6ece8123bdf03b3479667c
Muhammad <muhammadhullah23@gmail.com>
2021-12-11 22:06:50 -0500

    create Creat workout action

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -3,4 +3,13 @@ class WorkoutsController < ApplicationController
         workouts = Workout.all
         render json: workouts
     end
+
+    def create
+        @workout = Workout.new(workout_params)
+        if @workout.save
+            render json: @Workout
+        else
+            render json: {message: "Make sure to fill all inputs"}
+        end
+    end
 end

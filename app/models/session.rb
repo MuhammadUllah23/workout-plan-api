4be170328bb6e5e9a6ef7389c585912bee1729e1
Muhammad <muhammadhullah23@gmail.com>
2022-01-02 03:21:31 -0500

    Create session validations

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,11 @@
 class Session < ApplicationRecord
   belongs_to :workout
   belongs_to :exercise
+
+  validates :sets, presence: true, numericality: { greater_than: 0}
+  validates :reps, presence: true, numericality: { greater_than: 0}
+  validates :dropset, presence: true, numericality: { greater_than: 0}
+  validates :weight, numericality: { greater_than: 0}
+  validates :workout_id, presence: true
+  validates :exercise_id, presence: true
 end

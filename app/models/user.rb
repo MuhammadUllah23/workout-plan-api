d43cfee86134ab40f8c084b28efa3d965c2b17ef
Muhammad <muhammadhullah23@gmail.com>
2022-01-07 22:38:27 -0500

    username validates in User model
    presence is true and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
     has_many :workouts
+
+    has_secure_password
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :username, presence: true, uniqueness: { case_sensitive: false}
+    
 end

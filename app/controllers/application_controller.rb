5e63e77acf147818701424d60482b602e139440b
Muhammad <muhammadhullah23@gmail.com>
2022-01-08 00:57:28 -0500

    create encode_token function in ApplicationController
    Takes in payload as argument

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+    def encode_token(payload)
+        # payload => { beef: 'steak' }
+        JWT.encode(payload, 'my_s3cr3t')
+        # jwt string: "eyJhbGciOiJIUzI1NiJ9.eyJiZWVmIjoic3RlYWsifQ._IBTHTLGX35ZJWTCcY30tLmwU9arwdpNVxtVU0NpAuI"
+      end
 end

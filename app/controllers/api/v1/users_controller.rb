b8fe4d485da8a8a4cc804b145db0799b2ddd8ae0
Muhammad <muhammadhullah23@gmail.com>
2022-01-15 17:24:22 -0500

    update the UsersController
    update the UsersController so that it issues a token when users register for our app

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::UsersController < ApplicationController
     def create 
         @user = User.create(user_params)
         if @user.valid?
-            render json: @user, status: :created, location: @user
+            @token = encode_token(user_id: @user.id)
+            render json: {@user, jwt: @token}, status: :created, location: @user
         else
             render json: { error: 'failed to create user', message: "Make sure to fill all inputs" }, status: :unprocessable_entity
         end

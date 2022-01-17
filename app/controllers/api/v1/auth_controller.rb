9fda1880df67d86c94f4c8b24a11aa9a39abed93
Muhammad <muhammadhullah23@gmail.com>
2022-01-17 10:16:08 -0500

    Auth controller create action
    create action logins user

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::AuthController < ApplicationController
+
+    def create 
+        @user = User.find_by(username: user_login_params[:username])
+        if @user && @user.authenticate(user_login_params[:password])
+            token = encode_token({ user_id: @user.id})
+            render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+        else
+            render json: { message: "Invalid username or password" }, status: :unauthorized
+        end
+    end
 end

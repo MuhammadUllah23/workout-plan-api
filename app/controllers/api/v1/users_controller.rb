8789dedf4b76d064b92a029b8270386ef2300f14
Muhammad <muhammadhullah23@gmail.com>
2022-01-22 12:20:53 -0500

    user index action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: users
+    end
     def create 
         @user = User.create(user_params)
         # byebug

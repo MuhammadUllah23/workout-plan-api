96b043b199b45a4bf9cae7699c77bc4637b8461a
Muhammad <muhammadhullah23@gmail.com>
2022-01-27 16:54:01 -0500

    session params method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,11 @@ class SessionsController < ApplicationController
     def create
         @session = Session.create!(session_params)
     end
+
+    private
+
+    def session_params
+        params.require(:session).permit(:sets, :reps, :dropset, :weight, :workout_id, :exercise_id)
+    end
     
 end
